6fa7ffa86cdf10ead2aaa5376c34ba0355ae816d
Ernest <dite_malacoda@hotmail.com>
2022-04-14 12:37:34 -0500

    baseUrl añadida

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	// base: 'heroes-app',
+	base: '/heroes-app/',
 	build: {
 		outDir: 'docs'
 	}
